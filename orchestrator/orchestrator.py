d3f3caded07ae3fb2eed24ec64297bfa3c839edd
Shreyas BS <bsshreyas99@gmail.com>
2020-05-14 17:59:47 +0530

    rideshare_app_commented

--- orchestrator/orchestrator.py
+++ orchestrator/orchestrator.py
@@ -10,8 +10,14 @@ import datetime
 
 app = Flask(__name__)
 return_str = ""
+
+#Variable used to count read requests
 read_req=0
 
+
+#--------------------------------------------- APIs ------------------------------------------------------
+
+#Clear database
 @app.route("/api/v1/db/clear",methods=["POST"])
 def clear_db():
     sql_query = "DELETE FROM users"
@@ -20,37 +26,48 @@ def clear_db():
     replica_file.write(sql_query+"\n")
     replica_file.close()
     
+    #Publish query into writeQ
     channel.basic_publish(exchange='', routing_key='writeq', body=sql_query)
     return 'Cleared database',200
 
+
+#Delete specific rows in the database
 @app.route('/api/v1/db/delop',methods=["POST"])
 def del_db():
-    
     sql_query = request.get_json()['query']
     
     replica_file = open("replica.txt","a")
     replica_file.write(sql_query+"\n")
     replica_file.close()
     
+    #Publish query into writeQ
     channel.basic_publish(exchange='', routing_key='writeq', body=sql_query)
+
     return '',201
 
 
+#Write to the database
 @app.route('/api/v1/db/write',methods=["POST"])
 def write_db():
     to_ins = request.get_json()['insert'] # [ 'shashank', '6ae999552a0d2dca14d62e2bc8b764d377b1dd6c' ]
     to_col = request.get_json()['column'] #  [ 'username' , 'password' ]
     to_table = request.get_json()['table'] # users
 
+    #Construct sql query
     sql_query = "INSERT INTO "+to_table+" "+str(to_col).replace('[','(').replace(']',')').replace("'","")+" values "+str(to_ins).replace('[','(').replace(']',');')
 
+    #Write query into replica file
     replica_file = open("replica.txt","a")
     replica_file.write(sql_query+"\n")
     replica_file.close()
     
+    #Publish query into writeQ
     channel.basic_publish(exchange='', routing_key='writeq', body=sql_query)
+
     return '',201
 
+
+#Read from the database
 @app.route('/api/v1/db/read',methods=["POST"])
 def read_db():
     global read_req
@@ -58,6 +75,7 @@ def read_db():
     to_table = request.get_json()['table']
     to_conds = request.get_json()['where']
 
+    #Construct sql query
     sql_query = "SELECT " + str(to_col).replace('[',"").replace("]","").replace("'","") + " "
     sql_query += "FROM " + str(to_table) + " "
     if(len(to_conds)==1):
@@ -66,33 +84,42 @@ def read_db():
         sql_query += "WHERE " + str(to_conds).replace('[',"").replace("]","").replace("\"","").replace(","," and ") + ";"
     print(sql_query)
     
+    #Increment number of read requests
     read_req+=1
     
+    #Publish query into readQ
     channel.basic_publish(exchange='',
                       routing_key='readq',
                       body=sql_query,
                       properties=pika.BasicProperties(delivery_mode=2))
+
 
     def callback(ch,method,properties,body):
         global return_str
         return_str = body.decode('utf-8')
         channel.stop_consuming()
 
-    
+	#Consume from responseQ    
     channel.basic_consume(queue="responseq", on_message_callback=callback)
     channel.start_consuming()
+    
     print(return_str)
+    
+    #Return result
     if(eval(return_str)):
         return str(return_str),200
     abort(400)
 
 
+#Crash slave container
 @app.route('/api/v1/crash/slave',methods=["POST"])
 def crash_slave():
     to_ret = int(max([app_db_mapping[i][0].top()["Processes"][0][1] for i in app_db_mapping]))
     scale_in()
     return str([to_ret]),200
 
+
+#Crash master container
 @app.route('/api/v1/crash/master',methods=["POST"])
 def crash_master():
     to_kill = min([app_db_mapping[i][0].top()["Processes"][0][1] for i in app_db_mapping])
@@ -106,6 +133,8 @@ def crash_master():
     del(app_db_mapping[temp])
     return str([int(to_kill)]),200
 
+
+#List PIDs of all worker containers
 @app.route('/api/v1/worker/list',methods=["GET"])
 def list_worker():
     workers = []
@@ -113,8 +142,10 @@ def list_worker():
         workers.append(int(app_db_mapping[i][0].top()["Processes"][0][1]))
     return str(sorted(workers)),200
 
-def respawn_slave():
 
+
+#Spawn a new worker
+def respawn_slave():
     print("I am spawning a new worker")
     slave_db = client.containers.run("mysql_new:latest",environment=["MYSQL_ROOT_PASSWORD=root","MYSQL_DATABASE=cloud_ass3"],detach=True)
     slave_db_ip = client.containers.get(slave_db.attrs['Name']).attrs['NetworkSettings']['IPAddress']
@@ -129,16 +160,26 @@ def respawn_slave():
     print("New worker is deployed")        
     watch_worker()
 
+
+#------------------------------------------- Watch functions ----------------------------------------------
+
+#Watch function
 def inter_respawn(event):
     data, stat = zk.get("/election")
     if(int(data.decode('utf-8')) > len(zk.get_children("/election"))):
         t = threading.Thread(target=respawn_slave)
         t.start()
     
+
+#Set watch on previous znode in increasing order of PIDs
 def watch_worker():
     for i in zk.get_children("/election"):
         zk.exists("/election/"+str(i),watch=inter_respawn)
 
+
+#-------------------------------------------- Autoscaling ------------------------------------------------
+
+#Stop and remove worker with highest PID
 def scale_in():
     to_kill = max([app_db_mapping[i][0].top()["Processes"][0][1] for i in app_db_mapping])
     for i in app_db_mapping:
@@ -150,21 +191,32 @@ def scale_in():
             temp = i
     del(app_db_mapping[temp])
 
+
+#Scale based on number of read requests
 def autoscale():
     val = 0
     minutes = 0
     first_check = 0
     global read_req
     
+    #Wait for initial request to start autoscaling
     while(not first_check):
         first_check=read_req
 
-    print("found request to start")
+    print("Found request to start")
+
+    #Thread runs for 20 minutes
     while(minutes!=20):
+
+    	#Increment count every second
         if(not val == 120):
             val += 1
             time.sleep(1)
+
+        #After 2 minutes
         else:
+
+        	#Get number of workers present
             found = len(zk.get_children("/election"))
             
             inter_num= read_req
@@ -175,7 +227,11 @@ def autoscale():
 
             minutes += 2
             val=0
+
+            #Set new required number of workers
             zk.set("/election",str(req_count).encode())
+
+            #Autoscale
             if(found > req_count):
                 print("found {0} need {1} removing {2}".format(found,req_count,found-req_count))
                 for i in range(found - req_count):
@@ -188,20 +244,28 @@ def autoscale():
                     
     print("Stopping autoscaling")
     
+
+#---------------------------------------------- MAIN ----------------------------------------------------    
+
 if __name__=='__main__':
     
-    
+    #Get docker client
     client = docker.from_env()
     client2 = docker.APIClient(base_url='unix://var/run/docker.sock')
+
+    #Zookeeper client
     zk = KazooClient(hosts='172.17.0.3:2181')
     zk.start()
     zk.delete("/election",recursive=True)
 
+    #Mappings of slave application containers to corresponding database containers
     app_db_mapping = {}
     
+    #Add "election" znode
     if(not zk.ensure_path("/election")):
         zk.create("/election")
         
+    #Initial number of workers
     zk.set("/election",b"2")
 
     open("replica.txt","w").close()
@@ -210,6 +274,8 @@ if __name__=='__main__':
     rabbitmq_ip = '172.17.0.2'
     unique_id='1'
     container_maps={}
+
+    #Spawn 2 workers
     for _ in range(2):
         slave_db = client.containers.run("mysql_new:latest",environment=["MYSQL_ROOT_PASSWORD=root","MYSQL_DATABASE=cloud_ass3"],detach=True)
         slave_db_ip = client.containers.get(slave_db.attrs['Name']).attrs['NetworkSettings']['IPAddress']
@@ -221,6 +287,8 @@ if __name__=='__main__':
     print(container_maps)
     
     time.sleep(20)
+
+    #Copy sql commands and PID of worker into the container
     for i in container_maps:
         command = "echo "+str(container_maps[i].top()["Processes"][0][1])+" | cat > /app/mypid.txt"
         os.system("docker exec -i "+i+" /bin/bash -c '"+command+"'")
@@ -231,18 +299,22 @@ if __name__=='__main__':
     
     watch_worker()
 
+    #Thread for autoscaling
     print("Starting thread for autoscaling")
     t1 = threading.Thread(target=autoscale)
     t1.start()
     
-    
+    #RabbitMQ connection
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=str(rabbitmq_ip),heartbeat=0))
     channel = connection.channel()
 
+    #Declare readQ, writeQ, responseQ
     channel.queue_declare(queue='readq', durable=True)
     channel.queue_declare(queue='writeq', durable=True)
     channel.queue_declare(queue='responseq', durable=True)
+
+    #Running Flask app
     app.run(host='0.0.0.0',port=8000)
 
 
--- worker/worker.py
+++ worker/worker.py
@@ -5,16 +5,21 @@ import mysql.connector
 from kazoo.client import KazooClient
 import uuid
 
+#Wait for PID and replica files to be copied into container
 while(not os.path.exists("/app/mypid.txt") or not os.path.exists("/app/replica.txt")):
     pass
 
+#Read PID
 pid_file = open("/app/mypid.txt","r")
 pid = int(pid_file.readline())
 pid_file.close()
 
 rabbitmq_ip = os.environ["RABBITMQ_IP"]
+
+#Database IP
 mysql_db = os.environ["MYSQL_IP_ADDR"]
 
+#Connect to database container
 mydb = mysql.connector.connect(
     host = mysql_db,
     user="root",
@@ -25,22 +30,30 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 channel = None
 
+#Execute replica file to make database consistent
 for line in open("/app/replica.txt"):
     if(line):
         mycursor.execute(line)
         
 mydb.commit()
 
+#Zookeeper client
 zk = KazooClient(hosts="172.17.0.3:2181")
 zk.start()
 
+
+#-------------------------------------------- Watch functions ---------------------------------------------
+
+#Watch function
 def func(event):
-    
     global channel
     new_node2 = new_node.split("/")[2]
+
+    #Run master/leader code
     if(new_node2 == min(zk.get_children("/election"),key = lambda x:x.split("_")[1])):
         leader_function()
 
+    #Set watch on previous znode in increasing order of PIDs
     else:
         to_watch = "_-1"
         seq = new_node.split("_")[1]
@@ -51,9 +64,10 @@ def func(event):
 
         zk.exists("/election"+"/"+to_watch,watch=func)
 
-    
 
+#-------------------------------------- Master and Slave functions ----------------------------------------
 
+#Master function
 def leader_function():
     global channel
 
@@ -61,22 +75,26 @@ def leader_function():
     file.write("I am leader ")
     file.close()
 
-    
+    #RabbitMQ connection    
     connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=rabbitmq_ip,heartbeat=0))
     channel = connection.channel()
 
     channel.queue_declare(queue='writeq', durable=True)    
         
-    #declare syncQ
+    #Create exchange for syncQ
     channel.exchange_declare(exchange='sync', exchange_type='fanout')
     
     def callback(ch, method, properties, body):
         file = open("test.txt",'a')
         body = body.decode('utf-8')
-        #send sql query to sync Q
+
+        #Publish sql query into syncQ exchange
         channel.basic_publish(exchange='sync', routing_key='', body=body)
+
         file.write("Received "+body+'\n')
+
+        #Pass if database connection not established yet
         try:
             mycursor.execute(body)
             mydb.commit()
@@ -84,13 +102,20 @@ def leader_function():
             pass
         file.close()
             
+    #Consume from writeQ
     channel.basic_consume(queue="writeq", on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
         
+
+#Slave function
 def slave_function():
+
+    #RabbitMQ connection
     connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=rabbitmq_ip,heartbeat=0))
     channel = connection.channel()
+
+    #Connect to readQ and responseQ
     channel.queue_declare(queue='readq', durable=True)
     channel.queue_declare(queue='responseq', durable=True)
     
@@ -106,43 +131,67 @@ def slave_function():
         for i in mycursor.fetchall():
             res.append(i)
             
+        #Publish result into responseQ
         channel.basic_publish(exchange='', routing_key='responseq', body=str(res))
         
     def callback2(ch, method, properties, body):
         file = open("test_sync.txt",'a')
         body = body.decode('utf-8')
+
+        #Pass if database connection not established yet
         try:
             mycursor.execute(body)
             mydb.commit()
         except:
-            pass        
+            pass     
+
         file.write("Received "+body+'\n')
         file.close()
 
+    #Connect to sync exchange
     channel.exchange_declare(exchange='sync', exchange_type='fanout')
+
+    #Declare temporary syncQ
     result = channel.queue_declare(queue='', exclusive=True)
     queue_name = result.method.queue
+
+    #Bind syncQ to exchange
     channel.queue_bind(exchange='sync', queue=queue_name)
     
     channel.basic_qos(prefetch_count=1)
+
+    #Consume from readQ
     channel.basic_consume(queue="readq", on_message_callback=callback)
+
+    #Consume from syncQ
     channel.basic_consume(
         queue=queue_name, on_message_callback=callback2, auto_ack=True)
     channel.start_consuming()
 
+
+#--------------------------------------------------------------------------------------------------------
+
+#Execute master function
 if(len(zk.get_children("/election"))==0):
     unique_id = uuid.uuid4().hex+"_"+str(pid)
     new_node = zk.create("/election"+"/"+unique_id,ephemeral=True)
+
+    #################
     leader_function()
-        
+ 
+#Execute slave function       
 else:
     unique_id = uuid.uuid4().hex+"_"+str(pid)
     new_node = zk.create("/election"+"/"+unique_id,ephemeral=True)
     to_watch = "_-1"
     seq = new_node.split("_")[1]
+
+    #Set watch on previous znode in increasing order of PIDs 
     for i in zk.get_children("/election"):
         if(int(i.split("_")[1]) < int(seq) and int(i.split("_")[1]) > int(to_watch.split("_")[1])):
             to_watch = i
     print("Watching "+to_watch)
     zk.exists("/election"+"/"+to_watch,watch=func)
+
+    ################
     slave_function()
